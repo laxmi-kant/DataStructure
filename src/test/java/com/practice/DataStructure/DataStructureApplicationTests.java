2596d3333ba097102f4686b3152fa9b03dfe0967
lkyadav123 <laxmi.kant@srslive.in>
2019-12-03 18:27:33 +0530

    first commit

---
+++ src/test/java/com/practice/DataStructure/DataStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.DataStructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
